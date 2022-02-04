e81bd53cdd055903f115c80e092250f45e3d9276
Werayootk <werayoot5800@gmail.com>
2022-02-04 22:18:38 +0700

    Add Show/Hide Password

--- client/src/pages/Client/Edit/EditPassWord.js
+++ client/src/pages/Client/Edit/EditPassWord.js
@@ -1,63 +1,99 @@
 import React, { useState } from "react";
 import "./EditPassWord.scss";
-import { useFormik, Field, Form, Formik } from "formik";
-
+import { Field, Form, Formik } from "formik";
+import { EyeOutlined, EyeInvisibleOutlined } from "@ant-design/icons";
 const EditPassWord = () => {
-  const formikPass = useFormik({
-    initialValues: {
-      oldpassword: "",
-      newpassword: "",
-      confirmpassword: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+  const [ToggleOldPassword, setToggleOldPassword] = useState(false);
+  const [ToggleNewPassword, setToggleNewPassword] = useState(false);
+  const [ToggleConfirm, setToggleConfirm] = useState(false);
+
+  const toggleOldPasswordVisiblity = () => {
+    setToggleOldPassword(!ToggleOldPassword);
+  };
+
+  const toggleNewPasswordVisiblity = () => {
+    setToggleNewPassword(!ToggleNewPassword);
+  };
+
+  const toggleConPasswordVisiblity = () => {
+    setToggleConfirm(!ToggleConfirm);
+  };
+
   return (
     <div className="editpass">
       <div className="editpass_body">
         <h2>ตั้งรหัสผ่าน</h2>
-        <form onSubmit={formikPass.handleSubmit}>
-          <div className="editpass-form__group">
-            <div className="editpass-form__group-input">
-              <label htmlFor="oldpassword">รหัสผ่านปัจจุบัน</label>
-              <input
-                id="oldpassword"
-                name="oldpassword"
-                type= "password"
-                placeholder="ใส่รหัสผ่านปัจจุบัน"
-                onChange={formikPass.handleChange}
-                value={formikPass.values.oldpassword}
-              />
+        <Formik
+          initialValues={{
+            oldpassword: "",
+            newpassword: "",
+            confirmpassword: "",
+          }}
+          onSubmit={(values) => alert(JSON.stringify(values, null, 2))}
+        >
+          <Form>
+            <div className="editpass-form__group">
+              <div className="editpass-form__group-input">
+                <label htmlFor="oldpassword">รหัสผ่านปัจจุบัน</label>
+                <Field
+                  type={ToggleOldPassword ? "text" : "password"}
+                  id="oldpassword"
+                  name="oldpassword"
+                  className="form-control rounded-0"
+                  placeholder="ใส่รหัสผ่านปัจจุบัน"
+                />
+                {!ToggleOldPassword && (
+                  <EyeInvisibleOutlined className="oldpassword-icon" onClick={toggleOldPasswordVisiblity} />
+                )}
+                {ToggleOldPassword && (
+                  <EyeOutlined className="oldpassword-icon" onClick={toggleOldPasswordVisiblity} />
+                )}
+              </div>
             </div>
-            <div className="editpass-form__group-input">
-              <label htmlFor="newpassword">รหัสผ่านใหม่</label>
-              <input
-                id="newpassword"
-                name="newpassword"
-                type="password"
-                placeholder="ตั้งรหัสผ่านใหม่"
-                onChange={formikPass.handleChange}
-                value={formikPass.values.newpassword}
-              />
+            <div className="editpass-form__group">
+              <div className="editpass-form__group-input">
+                <label htmlFor="newpassword">รหัสผ่านใหม่</label>
+                <Field
+                  type={ToggleNewPassword ? "text" : "password"}
+                  id="newpassword"
+                  name="newpassword"
+                  className="form-control rounded-0"
+                  placeholder="ตั้งรหัสผ่านใหม่"
+                />
+                {!ToggleNewPassword && (
+                  <EyeInvisibleOutlined className="newpassword-icon" onClick={toggleNewPasswordVisiblity} />
+                )}
+                {ToggleNewPassword && (
+                  <EyeOutlined className="newpassword-icon" onClick={toggleNewPasswordVisiblity} />
+                )}
+              </div>
             </div>
-            <div className="editpass-form__group-input">
-              <label htmlFor="confirmpassword">รหัสผ่านใหม่อีกครั้ง</label>
-              <input
-                id="confirmpassword"
-                name="confirmpassword"
-                type="password"
-                placeholder="ใส่รหัสผ่านใหม่อีกครั้ง"
-                onChange={formikPass.handleChange}
-                value={formikPass.values.confirmpassword}
-              />
+            <div className="editpass-form__group">
+              <div className="editpass-form__group-input">
+                <label htmlFor="confirmpassword">รหัสผ่านใหม่อีกครั้ง</label>
+                <Field
+                  type={ToggleConfirm ? "text" : "password"}
+                  id="confirmpassword"
+                  name="confirmpassword"
+                  className="form-control rounded-0"
+                  placeholder="ใส่รหัสผ่านใหม่อีกครั้ง"
+                />
+                {!ToggleConfirm && (
+                  <EyeInvisibleOutlined className="confirmpassword-icon" onClick={toggleConPasswordVisiblity} />
+                )}
+                {ToggleConfirm && (
+                  <EyeOutlined className="confirmpassword-icon" onClick={toggleConPasswordVisiblity} />
+                )}
+              </div>
             </div>
-          </div>
-          <p className="warnning-text">
-            กรุณาตั้งรหัสผ่านที่มีอักขระ 8 ตัวขึ้นไป โดยใช้ตัวอักษร หรือตัวเลข
-          </p>
-          <button className="password-submit" type="submit">บันทึก</button>
-        </form>
+            <p className="warnning-text">
+              กรุณาตั้งรหัสผ่านที่มีอักขระ 8 ตัวขึ้นไป โดยใช้ตัวอักษร หรือตัวเลข
+            </p>
+            <button className="password-submit" type="submit">
+              บันทึก
+            </button>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
